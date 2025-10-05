f92d0ef063fcf56b989f0317ca3a5916bce320b7
winderica <winderica@gmail.com>
2025-10-06 03:37:15 +0800

    Refactor: new crate for traits

---
+++ lib/traits/src/lib.rs
@@ -0,0 +1,245 @@
+use ark_crypto_primitives::sponge::Absorb;
+use ark_ec::{
+    short_weierstrass::{Projective, SWCurveConfig},
+    AffineRepr, CurveGroup, PrimeGroup,
+};
+use ark_ff::{BigInteger, Field as ArkField, Fp, FpConfig, One, PrimeField, Zero};
+use ark_r1cs_std::{
+    fields::{fp::FpVar, FieldVar},
+    groups::{curves::short_weierstrass::ProjectiveVar, CurveVar},
+};
+use ark_relations::gr1cs::{ConstraintSystemRef, SynthesisError};
+
+pub type CF1<C> = <C as PrimeGroup>::ScalarField;
+pub type CF2<C> = <<C as CurveGroup>::BaseField as ArkField>::BasePrimeField;
+
+pub trait Dummy<Cfg> {
+    fn dummy(cfg: Cfg) -> Self;
+}
+
+impl<T: Default + Clone> Dummy<usize> for Vec<T> {
+    fn dummy(cfg: usize) -> Self {
+        vec![Default::default(); cfg]
+    }
+}
+
+impl<T: Default> Dummy<()> for T {
+    fn dummy(_: ()) -> Self {
+        Default::default()
+    }
+}
+
+/// Converts a value `self` into a vector of field elements, ordered in the same
+/// way as how a variable of type `Var` would be represented *natively* in the
+/// circuit.
+///
+/// This is useful for the verifier to compute the public inputs.
+pub trait Inputize<F> {
+    fn inputize(&self) -> Vec<F>;
+}
+
+/// Converts a value `self` into a vector of field elements, ordered in the same
+/// way as how a variable of type `Var` would be represented *non-natively* in
+/// the circuit.
+///
+/// This is useful for the verifier to compute the public inputs.
+///
+/// Note that we require this trait because we need to distinguish between some
+/// data types that are represented both natively and non-natively in-circuit
+/// (e.g., field elements can have type `FpVar` and `NonNativeUintVar`).
+pub trait InputizeNonNative<F> {
+    fn inputize_nonnative(&self) -> Vec<F>;
+}
+
+impl<F, T: Inputize<F>> Inputize<F> for [T] {
+    fn inputize(&self) -> Vec<F> {
+        self.iter().flat_map(Inputize::<F>::inputize).collect()
+    }
+}
+
+impl<F, T: InputizeNonNative<F>> InputizeNonNative<F> for [T] {
+    fn inputize_nonnative(&self) -> Vec<F> {
+        self.iter()
+            .flat_map(InputizeNonNative::<F>::inputize_nonnative)
+            .collect()
+    }
+}
+
+impl<P: FpConfig<N>, const N: usize> Inputize<Self> for Fp<P, N> {
+    /// Returns the internal representation in the same order as how the value
+    /// is allocated in `FpVar::new_input`.
+    fn inputize(&self) -> Vec<Self> {
+        vec![*self]
+    }
+}
+
+impl<P: SWCurveConfig<BaseField: Field>> Inputize<P::BaseField> for Projective<P> {
+    /// Returns the internal representation in the same order as how the value
+    /// is allocated in `ProjectiveVar::new_input`.
+    fn inputize(&self) -> Vec<P::BaseField> {
+        let affine = self.into_affine();
+        match affine.xy() {
+            Some((x, y)) => vec![x, y, One::one()],
+            None => vec![Zero::zero(), One::one(), Zero::zero()],
+        }
+    }
+}
+
+impl<F: Field, P: Field> InputizeNonNative<F> for P {
+    /// Returns the internal representation in the same order as how the value
+    /// is allocated in `NonNativeUintVar::new_input`.
+    fn inputize_nonnative(&self) -> Vec<F> {
+        self.into_bigint()
+            .to_bits_le()
+            .chunks(F::BITS_PER_LIMB)
+            .map(|chunk| F::from(F::BigInt::from_bits_le(chunk)))
+            .collect()
+    }
+}
+
+impl<P: SWCurveConfig<BaseField: Field, ScalarField: Field>> InputizeNonNative<P::ScalarField>
+    for Projective<P>
+{
+    /// Returns the internal representation in the same order as how the value
+    /// is allocated in `NonNativeAffineVar::new_input`.
+    fn inputize_nonnative(&self) -> Vec<P::ScalarField> {
+        let affine = self.into_affine();
+        let (x, y) = affine.xy().unwrap_or_default();
+
+        [x, y].inputize_nonnative()
+    }
+}
+
+/// `Field` trait is a wrapper around `PrimeField` that also includes the
+/// necessary bounds for the field to be used conveniently in folding schemes.
+pub trait Field:
+    PrimeField<BasePrimeField = Self> + Absorb + AbsorbNonNative + Inputize<Self>
+{
+    const BITS_PER_LIMB: usize;
+    /// The in-circuit variable type for this field.
+    type Var: FieldVar<Self, Self>;
+}
+
+impl<P: FpConfig<N>, const N: usize> Field for Fp<P, N> {
+    const BITS_PER_LIMB: usize = 55; // TODO: make this configurable
+    type Var = FpVar<Self>;
+}
+
+/// `Curve` trait is a wrapper around `CurveGroup` that also includes the
+/// necessary bounds for the curve to be used conveniently in folding schemes.
+pub trait Curve:
+    CurveGroup<ScalarField: Field, BaseField: Field>
+    + AbsorbNonNative
+    + Inputize<Self::BaseField>
+    + InputizeNonNative<Self::ScalarField>
+{
+    /// The in-circuit variable type for this curve.
+    type Var: CurveVar<Self, Self::BaseField>;
+}
+
+impl<P: SWCurveConfig<ScalarField: Field, BaseField: Field>> Curve for Projective<P> {
+    type Var = ProjectiveVar<P, FpVar<P::BaseField>>;
+}
+
+/// An interface for objects that can be absorbed by a `Transcript`.
+///
+/// Matches `Absorb` in `ark-crypto-primitives`.
+pub trait AbsorbNonNative {
+    /// Converts the object into field elements that can be absorbed by a `Transcript`.
+    /// Append the list to `dest`
+    fn to_native_sponge_field_elements<F: PrimeField>(&self, dest: &mut Vec<F>);
+
+    /// Converts the object into field elements that can be absorbed by a `Transcript`.
+    /// Return the list as `Vec`
+    fn to_native_sponge_field_elements_as_vec<F: PrimeField>(&self) -> Vec<F> {
+        let mut result = Vec::new();
+        self.to_native_sponge_field_elements(&mut result);
+        result
+    }
+}
+
+/// An interface for objects that can be absorbed by a `TranscriptVar` whose constraint field
+/// is `F`.
+///
+/// Matches `AbsorbGadget` in `ark-crypto-primitives`.
+pub trait AbsorbNonNativeGadget<F: PrimeField> {
+    /// Converts the object into field elements that can be absorbed by a `TranscriptVar`.
+    fn to_native_sponge_field_elements(&self) -> Result<Vec<FpVar<F>>, SynthesisError>;
+}
+
+impl<T: AbsorbNonNative> AbsorbNonNative for [T] {
+    fn to_native_sponge_field_elements<F: PrimeField>(&self, dest: &mut Vec<F>) {
+        for t in self.iter() {
+            t.to_native_sponge_field_elements(dest);
+        }
+    }
+}
+
+impl<F: PrimeField, T: AbsorbNonNativeGadget<F>> AbsorbNonNativeGadget<F> for &T {
+    fn to_native_sponge_field_elements(&self) -> Result<Vec<FpVar<F>>, SynthesisError> {
+        T::to_native_sponge_field_elements(self)
+    }
+}
+
+impl<F: PrimeField, T: AbsorbNonNativeGadget<F>> AbsorbNonNativeGadget<F> for [T] {
+    fn to_native_sponge_field_elements(&self) -> Result<Vec<FpVar<F>>, SynthesisError> {
+        let mut result = Vec::new();
+        for t in self.iter() {
+            result.extend(t.to_native_sponge_field_elements()?);
+        }
+        Ok(result)
+    }
+}
+
+impl<P: FpConfig<N>, const N: usize> AbsorbNonNative for Fp<P, N> {
+    fn to_native_sponge_field_elements<F: PrimeField>(&self, dest: &mut Vec<F>) {
+        let bits_per_limb = F::MODULUS_BIT_SIZE as usize - 1;
+        let num_limbs = (Fp::<P, N>::MODULUS_BIT_SIZE as usize).div_ceil(bits_per_limb);
+
+        let mut limbs = self
+            .into_bigint()
+            .to_bits_le()
+            .chunks(bits_per_limb)
+            .map(|chunk| F::from(F::BigInt::from_bits_le(chunk)))
+            .collect::<Vec<F>>();
+        limbs.resize(num_limbs, F::zero());
+
+        dest.extend(&limbs)
+    }
+}
+
+impl<P: SWCurveConfig<BaseField: Field>> AbsorbNonNative for Projective<P> {
+    fn to_native_sponge_field_elements<F: PrimeField>(&self, dest: &mut Vec<F>) {
+        let affine = self.into_affine();
+        let (x, y) = affine.xy().unwrap_or_default();
+
+        [x, y].to_native_sponge_field_elements(dest);
+    }
+}
+
+/// FCircuit defines the trait of the circuit of the F function, which is the one being folded (ie.
+/// inside the agmented F' function).
+/// The parameter z_i denotes the current state, and z_{i+1} denotes the next state after applying
+/// the step.
+/// Note that the external inputs for the specific circuit are defined at the implementation of
+/// both `FCircuit::ExternalInputs` and `FCircuit::ExternalInputsVar`, where the `Default` trait
+/// implementation for the `ExternalInputs` returns the initialized data structure (ie. if the type
+/// contains a vector, it is initialized at the expected length).
+pub trait FCircuit<F: PrimeField> {
+    type ExternalInputs;
+
+    /// returns the number of elements in the state of the FCircuit, which corresponds to the
+    /// FCircuit inputs.
+    fn state_len(&self) -> usize;
+
+    /// generates the constraints for the step of F for the given z_i
+    fn generate_step_constraints(
+        // this method uses self, so that each FCircuit implementation (and different frontends)
+        // can hold a state if needed to store data to generate the constraints.
+        &self,
+        cs: ConstraintSystemRef<F>,
+        i: usize,
+        z_i: Vec<FpVar<F>>,
+        external_inputs: Self::ExternalInputs, // inputs that are not part of the state
+    ) -> Result<Vec<FpVar<F>>, SynthesisError>;
+}
